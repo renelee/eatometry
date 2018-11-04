a12d55d9de09f5a2c3abd89f9bc7363f0b658493
Rene Lee <renelee@berkeley.edu>
2018-11-03 17:53:07 -0700

    redoing app.py

--- app.py
+++ app.py
@@ -1,25 +1,36 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from flask import send_from_directory
+# from PIL import Image
+import pytesseract
+
+UPLOAD_FOLDER = '/uploads/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
-
-UPLOAD_FOLDER = os.path.basename('uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+# function to get the unwanted ingredients from user
+@app.route('/', methods=['POST'])
+def my_form_post():
+	text = request.form['text']
+    # processed_text = text.upper()
+    return processed_text
 
-# @app.route('/upload', methods=['POST'])
-# def upload_file():
-#     file = request.files['image']
-#     f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-    
-#     # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
-#     file.save(f)
 
-#     return render_template('index.html')
+@app.route('/', methods=['POST'])
+def get_ingredients_from_image(path_to_image):
+	ingredients = pytesseract.image_to_string(Image.open(path_to_image))
+	unwanted_ingredients = set(request.form['unwanted_ingredients'])
+	results = []
+	for u in unwanted_ingredients:
+		if u in ingredients:
+			results.append(u)
+	return render_template('', ingredients=results)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
