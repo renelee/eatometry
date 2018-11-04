ffcd97711837299bd3f304524ef15e3cc1f3f7b9
Rene Lee <renelee@berkeley.edu>
2018-11-03 19:29:43 -0700

    stuff

--- app.py
+++ app.py
@@ -10,27 +10,23 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+@app.route('/')
+def home():
+	return render_template('index.html')
 
-# function to get the unwanted ingredients from user
-@app.route('/', methods=['POST'])
-def my_form_post():
-	text = request.form['text']
-    # processed_text = text.upper()
-    return processed_text
-
-
-@app.route('/', methods=['POST'])
-def get_ingredients_from_image(path_to_image):
-	ingredients = pytesseract.image_to_string(Image.open(path_to_image))
-	unwanted_ingredients = set(request.form['unwanted_ingredients'])
-	results = []
-	for u in unwanted_ingredients:
-		if u in ingredients:
-			results.append(u)
-	return render_template('results.html', ingredients=results)
-
-
-
+@app.route('/upload', methods=['POST'])
+def get_ingredients_from_image():
+	if request.method == 'POST':
+		# if the request method is post, then we get the form image and unwanted ingredients
+		path_to_image = request.form['image']
+		ingredients = pytesseract.image_to_string(Image.open(path_to_image))
+		unwanted_ingredients = set(request.form['unwanted_ingredients'])
+		results = []
+		for u in unwanted_ingredients:
+			if u in ingredients:
+				results.append(u)
+		return render_template('results.html', ingredients=results)
+	return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
